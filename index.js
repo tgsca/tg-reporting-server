3ef8e319d41008882bc5941d5841a36e165a440f
Andreas <andreas.sczepanski@testgilde.de>
2018-10-23 05:25:39 +0200

    CORS installed

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const winston = require('winston');
-const express = require('express');
+const winston = require("winston");
+const express = require("express");
 const app = express();
 
-require('./startup/prod')(app);
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/logging')();
-require('./startup/config')();
+require("./startup/prod")(app);
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/logging")();
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => winston.info(`Listening to port ${port}...`));
+const server = app.listen(port, () =>
+    winston.info(`Listening to port ${port}...`)
+);
 
 module.exports = server;
